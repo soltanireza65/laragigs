4bd598e7c94a8888a0bcc15d8cae1b1da640ab89
Reza Soltani <soltanireza65@gmail.com>
2023-12-29 22:29:40 +0330

    configure seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Jone Doe',
+            'email' => 'john@gmail.com',
+        ]);
+        Listing::factory(6)->create([
+            'user_id' => $user->id,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
